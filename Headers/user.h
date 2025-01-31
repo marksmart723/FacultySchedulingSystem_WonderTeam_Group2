9f7047b0ce43fa6559f5089bb335c6e936a232a6
mark smart <smartm2@student.douglascollege.ca>
2025-01-31 15:54:33 -0800

    added  user class and header

---
+++ Headers/user.h
@@ -0,0 +1,22 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+class User {
+private:
+    std::string username;
+    std::string password;
+    std::string role;
+
+public:
+    bool addUser(const std::string& uname, const std::string& pwd, const std::string& rl);
+    bool login(const std::string& uname, const std::string& pwd);
+    std::string getRole();
+    std::string getUsername();
+    std::string getPassword();
+    void changeRole(std::string& rl);
+    bool isLoggedIn();
+};
+
+#endif
