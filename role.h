a255e47d9addf66b57f8c5d134fe59c9656955f2
NatalieDCAcc <ermakn@student.douglascollege.ca>
2025-02-06 22:05:38 -0800

    Added functionality to change current role

--- role.h
+++ role.h
@@ -12,23 +12,26 @@ enum roleName
 class Role
 {
 private:
-	std::string role;
-	unsigned id;
-	std::vector<std::string> permitionNames;
-	std::vector<bool> permitions;
-	std::map<std::string, roleName> roleMap;
+	std::string role;							// name of the Role (should match exactly the role name in data text file)
+	unsigned id;								// role id according to enum oleName
+	std::vector<std::string> permitionNames;	// vector with permition names
+	std::vector<bool> permitions;				// vector with bool values according to existing permitions for the role
+	std::map<std::string, roleName> roleMap;	// maps std::string role with role id
 
 public:
-    // default constructor, creates a Role object with minimum permitions (TempFaculty)
+    // default constructor, creates an empty Role object with no permitions 
     Role();
     // parametrized constructor, uses string rName representing the role to create a Role object with the according permitions 
 	Role(std::string);
 	
-    // accessor. Returns a vector of strings of permittions
-	std::vector<std::string> getPermitions();
+    // accessor. Returns a const vector of strings of permitions
+	const std::vector<std::string> getPermitions() const;
 	
-	// accessor. Returns a string with the current Role
-	std::string getRole() const;
+	// accessor. Returns a const string with the current Role
+	const std::string getRole() const;
+
+	// takes a string newRole as argument, updates the role and permitions
+	bool changeRole(std::string);
 
 };
 
